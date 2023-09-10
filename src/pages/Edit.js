f6ae3db79b8c0077f9a953c7e083cdbd17f06842
seohyundeveloper <seoandjisister77@gmail.com>
2023-09-11 01:14:53 +0900

    폰트, 레이아웃, 공통 컴포넌트 작업

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -8,7 +8,7 @@ const Edit = () => {
   const id = searchParams.get('id');
   const mode = searchParams.get('mode');
 
-  return (
+  return (  
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지 입니다.</p>

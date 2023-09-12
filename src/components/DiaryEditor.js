02d9a29ebe483113e54f067086ff0035cf6e9692
seohyundeveloper <seoandjisister77@gmail.com>
2023-09-13 03:21:36 +0900

    최적화하기- React.memo이용

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useContext, useEffect } from "react";
+import { useState, useRef, useContext, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import MyButton from "./MuButton";
 import MyHeader from "./MyHeader";
@@ -21,9 +21,9 @@ const DiaryEditor = ({isEdit, originData}) => {
 
   const navigate = useNavigate();
 
-  const handleClickEmote = (emotion) => {
+  const handleClickEmote = useCallback((emotion) => {
     setEmotion(emotion);
-  };
+  }, []);
 
   const handleSubmit = () => {
     if(content.length < 1) {

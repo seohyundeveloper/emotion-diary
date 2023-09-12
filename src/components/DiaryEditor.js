4d5be36c91805bf2add0f6674faf7a168213f9df
seohyundeveloper <seoandjisister77@gmail.com>
2023-09-13 01:18:25 +0900

    일기 상세페이지 구현하기

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -4,41 +4,11 @@ import MyButton from "./MuButton";
 import MyHeader from "./MyHeader";
 import EmotionItem from "./EmotionItem";
 import { DiaryDispatchContext } from "../App";
+import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotion";
 
 const env = process.env;
 env.PUBLIC_URL = env.PUBLIC_URL || " ";
-
-const emotionList = [
-  {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_descript: '완전 좋음'
-  },
-  {
-    emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-    emotion_descript: '좋음'
-  },
-  {
-    emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_descript: '그럭저럭'
-  },
-  {
-    emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: '나쁨'
-  },
-  {
-    emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-    emotion_descript: '완전 나쁨'
-  }
-]
-
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
 
 const DiaryEditor = ({isEdit, originData}) => {
   const [date, setDate] = useState(getStringDate(new Date()));
@@ -52,8 +22,8 @@ const DiaryEditor = ({isEdit, originData}) => {
   const navigate = useNavigate();
 
   const handleClickEmote = (emotion) => {
-    setEmotion(emotion)
-  }
+    setEmotion(emotion);
+  };
 
   const handleSubmit = () => {
     if(content.length < 1) {
@@ -67,7 +37,6 @@ const DiaryEditor = ({isEdit, originData}) => {
         onEdit(originData.id, date, content, emotion);
       }
     }
-    
     navigate('/', { replace: true });
   }
 
